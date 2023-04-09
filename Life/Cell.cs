6ca5e43e9941a50a327a3e16857d471657b483c4
Pachurina Darya <126852495+skyroro@users.noreply.github.com>
2023-04-09 21:49:28 +0300

    Create Cell.cs

---
+++ Life/Cell.cs
@@ -0,0 +1,26 @@
+using System;
+namespace life
+{
+    public class Cell //ячейка
+    {
+        public bool IsAlive; //живой
+        public readonly List<Cell> neighbors = new List<Cell>(); //соседи
+
+        private bool IsAliveNext; //жив в следующем поколении
+
+        public void DetermineNextLiveState() //определить следующее состояние
+        {
+            int liveNeighbors = neighbors.Where(x => x.IsAlive).Count(); //определяяется кол-во живых соседей
+
+            if (IsAlive) //если он сейчас жив, нужно два или три соседа чтобы продолжал жить
+                IsAliveNext = liveNeighbors == 2 || liveNeighbors == 3;
+            else //если сейчас мертв, родится если рядом ровно три живые клетки
+                IsAliveNext = liveNeighbors == 3;
+        }
+
+        public void Advance() //переходим как бы в следующее поколение
+        {
+            IsAlive = IsAliveNext;
+        }
+    }
+}
